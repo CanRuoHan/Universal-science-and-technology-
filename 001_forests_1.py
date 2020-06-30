271eae945219d46a2e096afb8dd019f23b66fc46
hanshao1314 <54384461+hanshao1314@users.noreply.github.com>
2020-06-30 09:22:24 +0800

    Update 001_forests_1.py

--- 001_forests_1.py
+++ 001_forests_1.py
@@ -4,10 +4,6 @@ from sklearn.ensemble import IsolationForest
 import warnings
 warnings.filterwarnings("ignore")  #消除警告
 
-def coacat_columns_values(series):
-    string = '—'.join(map(str, series))
-    return string
-
 def iforests_model(data,columns):
     columns_yd=columns[:5]
     ifore=IsolationForest(n_estimators=100,max_samples="auto",contamination=0.1)
@@ -18,7 +14,24 @@ def iforests_model(data,columns):
     print(data.shape)
     # data.to_csv("res_data.csv",index=False,mode="a")
 
-    print("finish>>>>>>>>>")
+    est_dict, est_values, = {}, []
+    sort_dict, sort_values = {}, []
+    count = 0
+    for col, tree in zip(ifore.estimators_features_, ifore.estimators_):
+        count += 1
+        for i, j, k in zip(col, tree.feature_importances_, columns_yd):
+            est_dict.update({str(i).replace(str(i), k): float('%.8f' % (j))})  # 存放每一个维度对应的feature
+        est_values.append(est_dict)
+        sort_res = sorted(est_dict.items(), key=lambda d: d[1], reverse=True)
+        print("这是第【{}】棵树，按照其特征从大到小排列：\n【{}】".format(count, sort_res))
+    est_res = pd.DataFrame(est_values)
+    mean = pd.DataFrame(est_res.loc[:, est_res.columns].mean()).rename(columns={0: "mean"}).sort_values(by="mean",
+              axis=0,ascending=False)
+    print("每个特征名称的均值是：\n【{}】".format(mean))
+
+    """
+    需求：将对应的平均数mean,替换结果为-1，所对应的原来的学习数据，五个字段的值。
+    """
 
 def load_data():
     data = pd.read_csv("./iforests_data_1.csv", encoding="ansi")
